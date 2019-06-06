d1022c613f8a939b487be8efb5fb5b742777d168
Jedi18 <targetakhil@gmail.com>
2019-06-06 11:50:41 +0530

    Added models and applied inital migration

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import PizzaAndSubs, Others, ToppingsAndExtra
 # Register your models here.
+admin.site.register(PizzaAndSubs)
+admin.site.register(Others)
+admin.site.register(ToppingsAndExtra)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class PizzaAndSubs(models.Model):
+    name = models.CharField(max_length=64)
+    type = models.CharField(max_length=64)
+    small = models.DecimalField(decimal_places=2, max_digits=8)
+    large = models.DecimalField(decimal_places=2, max_digits=8)
+    ispizza = models.BooleanField()
+
+class Others(models.Model):
+    name = models.CharField(max_length=64)
+    type = models.CharField(max_length=64)
+    small = models.DecimalField(decimal_places=2, max_digits=8, null=True)
+    large = models.DecimalField(decimal_places=2, max_digits=8)
+    isdinnerplatter = models.BooleanField()
+
+class ToppingsAndExtra(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=8, null=True)
+    isextra = models.BooleanField()
