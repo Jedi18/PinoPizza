7ba2d885316c07605e7c2cec26cebc100cd94b46
Jedi18 <targetakhil@gmail.com>
2019-06-08 14:55:41 +0530

    updated index page

--- orders/static/orders/js/index.js
+++ orders/static/orders/js/index.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#selectedpizzaandsub').style.visibility = "";
+  document.querySelector('#selectedothers').style.visibility = "hidden";
+  
   document.querySelector('#pizzaorothers').onchange = function(){
-    
-  }
+    var selected = document.querySelector('#pizzaorothers').value;
+
+    if(selected == "pizzaandsub")
+    {
+      document.querySelector('#selectedpizzaandsub').style.visibility = "";
+      document.querySelector('#selectedothers').style.visibility = "hidden";
+    }
+    else if(selected=="others")
+    {
+      document.querySelector('#selectedpizzaandsub').style.visibility = "hidden";
+      document.querySelector('#selectedothers').style.visibility = "";
+    }
+  };
 });
