158708b738bcadf2adf6a26ab8c6e193066b736e
Jedi18 <targetakhil@gmail.com>
2019-06-17 02:01:06 +0530

    Added order table to models and applied migrations

--- orders/models.py
+++ orders/models.py
@@ -19,3 +19,10 @@ class ToppingsAndExtra(models.Model):
     name = models.CharField(max_length=64)
     price = models.DecimalField(decimal_places=2, max_digits=8, null=True, blank=True)
     isextra = models.BooleanField()
+
+class Orders(models.Model):
+    order = models.CharField(max_length=128)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    cartno = models.IntegerField()
+    toppingorextra = models.CharField(max_length=64, null=True, blank=True)
+    size = models.CharField(max_length=64, null=True, blank=True)
